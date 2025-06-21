d551f5d8b8112ff8314ce6018ffb0a2e469a3423
Danny <gbnguyenw@gmail.com>
2025-06-21 15:00:50 -0400

    vercel deployment initial commit

---
+++ mypythonshoppingcartapi/app/api/index.py
@@ -0,0 +1,11 @@
+from app.api import app
+from flask import Flask
+from app.swaggerapi import swaggerapp
+
+application = Flask(__name__)
+
+application.register_blueprint(app, url_prefix="/api")
+application.register_blueprint(swaggerapp, url_prefix="/docs")
+
+if __name__ == "__main__":
+    application.run(debug=True)
